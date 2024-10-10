29eb237b329d5be29da5dcee5b2a7618a1efec09
Anuj Singh <anuj.s@mindfiresolutions.com>
2024-10-10 13:28:11 +0530

    fix: constant import error

--- serverside/routes/user/fetchStateAndCity.js
+++ serverside/routes/user/fetchStateAndCity.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import constant from '../../utils/constant';
+import  constant from '../../utils/constant.js';
 
 export const getState = async (req, res) => {
     try {
         const response = await axios.get('https://cdn-api.co-vin.in/api/v2/admin/location/states');
-        res.json(response.data);
+        return res.json(response.data);
     } catch (error) {
         return res.status(400).send({ status: false, message: constant.general.genericError });
     }
@@ -14,7 +14,7 @@ export const getCity = async (req, res) => {
     const { stateId } = req.params;
     try {
         const response = await axios.get(`https://cdn-api.co-vin.in/api/v2/admin/location/districts/${stateId}`);
-        res.json(response.data);
+        return res.json(response.data);
     } catch (error) {
         return res.status(400).send({ status: false, message: constant.general.genericError });
     }
